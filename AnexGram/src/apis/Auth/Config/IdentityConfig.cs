356f56eb4ce1321efe8b80914a1444928586e6e9
Encina Javier <encinajavier7@gmail.com>
2019-02-26 23:19:11 -0300

    Add IdentityServer4

---
+++ AnexGram/src/apis/Auth/Config/IdentityConfig.cs
@@ -0,0 +1,67 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Config
+{
+    public static class IdentityConfig
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("CoreApi", "My Anexgram core API")
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients(
+           IConfiguration configuration
+       )
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = configuration.GetValue<string>("Client:ClientId"),
+                    ClientName = "Back-office client",
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+                    RequireConsent = false,
+
+                    ClientSecrets =
+                    {
+                        new Secret(configuration.GetValue<string>("Client:SecretKey").Sha256())
+                    },
+
+                    RedirectUris           = { $"signin-oidc" },
+                    PostLogoutRedirectUris = { $"signout-callback-oidc" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "CoreApi",
+                    },
+
+                    AllowOfflineAccess = true,
+
+                    // Envía el token por defecto
+                    AlwaysIncludeUserClaimsInIdToken = true
+                }
+            };
+        }
+    }
+}
---
+++ AnexGram/src/apis/Auth/Models/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auth.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
